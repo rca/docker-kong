514a0a2df27772d2d46c4908885710db4da91b40
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2016-07-15 11:15:51 -0400

    Check for DATABASE_HOST env var
    Set the database host if passed in the environment.  This is useful for
    deploying this container out to Kubernetes where there is some issue
    between dnsmasq and the Kubernetes resolver when trying to access a
    service.
    
    https://github.com/Mashape/kong/issues/1104

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ KONG_CONFIG=/etc/kong/kong.yml
 if [ -n "$DATABASE" ]; then
   echo -e '\ndatabase: "'$DATABASE'"' >> ${KONG_CONFIG}
 fi
+
+# Update the host if given
+if [ -n "${DATABASE_HOST}" ]; then
+  sed -i -e "s/host: \"kong-database\"/host: \"${DATABASE_HOST}\"/" ${KONG_CONFIG}
+fi;
