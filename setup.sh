8221654b612f44e6beaef5f91f38f1c3a0ecdd7d
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2016-07-15 11:15:41 -0400

    Refactored out KONG_CONFIG

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+KONG_CONFIG=/etc/kong/kong.yml
+
 # Setting up the proper database
 if [ -n "$DATABASE" ]; then
-  echo -e '\ndatabase: "'$DATABASE'"' >> /etc/kong/kong.yml
+  echo -e '\ndatabase: "'$DATABASE'"' >> ${KONG_CONFIG}
 fi
